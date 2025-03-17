ce6f5ebd0e36186d4fb0425d6b34ec9b3b5da67c
Mohammad Rezaei <mohrezaeib@gmail.com>
2025-03-17 12:18:27 +0100

    add parser

---
+++ app/cyto_db_shiny_app/parse_sdf.py
@@ -0,0 +1,35 @@
+# parse_sdf.py
+import base64
+import io
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import rdDepictor
+import json
+rdDepictor.SetPreferCoordGen(True)
+
+def parse_sdf_to_data(sdf_path):
+    suppl = Chem.SDMolSupplier(sdf_path, sanitize=True, removeHs=True, strictParsing=True)
+    results = []
+    mol_idx = 0
+
+    for mol in suppl:
+        if mol is None:
+            continue
+        Chem.rdDepictor.Compute2DCoords(mol)
+        img = Draw.MolToImage(mol, size=(1000,1000))
+        buf = io.BytesIO()
+        img.save(buf, format='PNG')
+        b64 = base64.b64encode(buf.getvalue()).decode('utf-8')
+
+        fields_dict = {}
+        for prop_name in mol.GetPropNames():
+            fields_dict[prop_name] = mol.GetProp(prop_name)
+
+        results.append({'mol_idx': mol_idx, 'base64_png': b64, 'fields': fields_dict})
+        mol_idx += 1
+
+    return results
+
+result = parse_sdf_to_data("CytoLabs_Database.sdf")
+with open("data.json", "w") as file:
+    json.dump(result, file)
